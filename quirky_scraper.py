229c1f417bd4cc7fafdc52e2b4107b852a9695d0
hcollins345 <hcollins345@gmail.com>
2018-12-17 22:19:50 +0100

    made notes file and fixed bugs in quirky_scraper.py

--- quirky_scraper.py
+++ quirky_scraper.py
@@ -8,10 +8,27 @@ import re
 
 initial_url = 'https://invent.quirky.com/api/v1/user_profile/765514/following?paginated_options%5Bfollows%5D%5Buse_cursor%5D=true&paginated_options%5Bfollows%5D%5Bper_page%5D=12&paginated_options%5Bfollows%5D%5Border_column%5D=created_at&paginated_options%5Bfollows%5D%5Border%5D=des'
 
-def make_url(user_id, ending=''):
-    section = 'https://invent.quirky.com/api/v1/user_profile/'
-    section2 = '/following?paginated_options%5Bfollows%5D%5Buse_cursor%5D=true&paginated_options%5Bfollows%5D%5Bper_page%5D=12&paginated_options%5Bfollows%5D%5Border_column%5D=created_at&paginated_options%5Bfollows%5D%5Border%5D=des'
+
+def make_url(user_id, ending=None):
+    '''
+    returns url for a page in followers or following of the user profile. 
+
+    PARAMETER
+    ----------
+    user_id: the user whoes page you are on
+    ending: leave blank for first page, for other pages insert the last
+        relationship's created_by attribute
+    '''
+    section1 = 'https://invent.quirky.com/api/v1/user_profile/'
+    user_id = str(user_id)
+    section2 = '/following?paginated_options%5Bfollows%5D%5Buse_cursor%5D=true&paginated_options%5Bfollows%5D%5Bper_page%5D=12&paginated_options%5Bfollows%5D%5Border_column%5D=created_at&paginated_options%5Bfollows%5D%5Border%5D=desc'
+    if ending == None:
+        ending = ''
+    else:
+        section3 = '&paginated_options%5Bfollows%5D%5Bcursor%5D='
+        ending = section3 + ending
+
     url = section1 + user_id + section2 + ending
     return url
 
-print(make_url(5657))
+print(make_url('164569'))
